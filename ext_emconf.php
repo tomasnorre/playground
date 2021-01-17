e35920cb8bbf8e539bbaa0c3ff611726f909692c
Tomas Norre Mikkelsen <tomas.mikkelsen@aoe.com>
2021-01-17 19:54:09 +0100

    add ext_emconf

---
+++ ext_emconf.php
@@ -0,0 +1,21 @@
+<?php
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'PLayground',
+    'description' => '',
+    'category' => 'module',
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'author' => 'Tomas Norre Mikkelsen',
+    'author_email' => '',
+    'author_company' => '',
+    'version' => '0.0.5',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ]
+];
